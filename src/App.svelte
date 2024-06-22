cfb4a32fa9f7730b8adcf822fcb44053c1f6ede5
spykelionel <social.lionel@gmail.com>
2024-06-22 09:38:28 +0100

    refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,6 @@
 <script>
-	export let name;
-	const toggleName = _=>{
-		if(name=="world"){
-			name = "SERTER"
-		} else {
-			name = "world"
-		}
-	}
+  import Todos from "./components/Todos.svelte";
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<button on:click={toggleName} type="button">Toggle Name</button>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	button {
-		background-color: #ff3e00;
-		color: white;
-	}
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Todos/>
