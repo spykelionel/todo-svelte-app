a56b6ef247db4f1c9c8964589e32a99aa275e43c
spykelionel <social.lionel@gmail.com>
2024-06-22 10:17:35 +0100

    update: add todo, remove, filter, and mark as completed

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,80 +1,127 @@
 <script>
-    export let todos = [];
-    let totalTodos = todos.length;
-    let completedTodos = todos.filter((todo) => todo.completed).length;
-  </script>
-  
+  export let todos = [];
+  $: totalTodos = todos.length;
+  $: completedTodos = todos.filter((todo) => todo.completed).length;
+  function removeTodo(todo) {
+    todos = todos.filter((t) => t.id !== todo.id);
+  }
+  let newTodo = "";
+  function addTodo() {
+    if (newTodo.length < 1) {
+      alert("Field can not be empty");
+      return;
+    }
+    const value = { completed: false, id: todos.length + 1, name: newTodo };
+    todos = [...todos, value];
+    newTodo = "";
+  }
+
+  let filter = "all";
+  const filterTodos = (filter, todos) =>
+    filter === "active"
+      ? todos.filter((t) => !t.completed)
+      : filter === "completed"
+        ? todos.filter((t) => t.completed)
+        : todos;
+</script>
 
 <!-- Todos.svelte -->
 <div class="todoapp stack-large">
-    <!-- NewTodo -->
-    <form>
-      <h2 class="label-wrapper">
-        <label for="todo-0" class="label__lg"> What needs to be done? </label>
-      </h2>
-      <input type="text" id="todo-0" autocomplete="off" class="input input__lg" />
-      <button type="submit" disabled="" class="btn btn__primary btn__lg">
-        Add
-      </button>
-    </form>
-  
-    <!-- Filter -->
-    <div class="filters btn-group stack-exception">
-      <button class="btn toggle-btn" aria-pressed="true">
-        <span class="visually-hidden">Show</span>
-        <span>All</span>
-        <span class="visually-hidden">tasks</span>
-      </button>
-      <button class="btn toggle-btn" aria-pressed="false">
-        <span class="visually-hidden">Show</span>
-        <span>Active</span>
-        <span class="visually-hidden">tasks</span>
-      </button>
-      <button class="btn toggle-btn" aria-pressed="false">
-        <span class="visually-hidden">Show</span>
-        <span>Completed</span>
-        <span class="visually-hidden">tasks</span>
-      </button>
-    </div>
-  
-    <!-- TodosStatus -->
-    <h2 id="list-heading">{completedTodos} out of {totalTodos} items completed</h2>  
-    <!-- Todos -->
-    <!-- svelte-ignore a11y-no-redundant-roles -->
-   <!-- To-dos -->
-<ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-    {#each todos as todo (todo.id)}
-    <li class="todo">
-      <div class="stack-small">
-        <div class="c-cb">
-          <input
-            type="checkbox"
-            id="todo-{todo.id}"
-            checked={todo.completed} />
-          <label for="todo-{todo.id}" class="todo-label"> {todo.name} </label>
+  <!-- NewTodo -->
+  <form on:submit|preventDefault={addTodo}>
+    <h2 class="label-wrapper">
+      <label for="todo-0" class="label__lg"> What needs to be done? </label>
+    </h2>
+    <input
+      bind:value={newTodo}
+      type="text"
+      id="todo-0"
+      autocomplete="off"
+      class="input input__lg"
+    />
+    <button type="submit" disabled="" class="btn btn__primary btn__lg">
+      Add
+    </button>
+  </form>
+
+  <!-- Filter -->
+  <div class="filters btn-group stack-exception">
+    <button
+      class="btn toggle-btn"
+      class:btn__primary={filter === "all"}
+      aria-pressed={filter === "all"}
+      on:click={() => (filter = "all")}
+    >
+      <span class="visually-hidden">Show</span>
+      <span>All</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+    <button
+      class="btn toggle-btn"
+      class:btn__primary={filter === "active"}
+      aria-pressed={filter === "active"}
+      on:click={() => (filter = "active")}
+    >
+      <span class="visually-hidden">Show</span>
+      <span>Active</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+    <button
+      class="btn toggle-btn"
+      class:btn__primary={filter === "completed"}
+      aria-pressed={filter === "completed"}
+      on:click={() => (filter = "completed")}
+    >
+      <span class="visually-hidden">Show</span>
+      <span>Completed</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+  </div>
+
+  <!-- TodosStatus -->
+  <h2 id="list-heading">
+    {completedTodos} out of {totalTodos} items completed
+  </h2>
+  <!-- Todos -->
+  <!-- svelte-ignore a11y-no-redundant-roles -->
+  <!-- To-dos -->
+  <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
+    {#each filterTodos(filter, todos) as todo (todo.id)}
+      <li class="todo">
+        <div class="stack-small">
+          <div class="c-cb">
+            <input
+              type="checkbox"
+              id="todo-{todo.id}"
+              on:click={() => (todo.completed = !todo.completed)}
+              checked={todo.completed}
+            />
+            <label for="todo-{todo.id}" class="todo-label"> {todo.name} </label>
+          </div>
+          <div class="btn-group">
+            <button type="button" class="btn">
+              Edit <span class="visually-hidden">{todo.name}</span>
+            </button>
+            <button
+              on:click={() => removeTodo(todo)}
+              type="button"
+              class="btn btn__danger"
+            >
+              Delete <span class="visually-hidden">{todo.name}</span>
+            </button>
+          </div>
         </div>
-        <div class="btn-group">
-          <button type="button" class="btn">
-            Edit <span class="visually-hidden">{todo.name}</span>
-          </button>
-          <button type="button" class="btn btn__danger">
-            Delete <span class="visually-hidden">{todo.name}</span>
-          </button>
-        </div>
-      </div>
-    </li>
+      </li>
     {:else}
-    <li>Nothing to do here!</li>
+      <li>Nothing to do here!</li>
     {/each}
   </ul>
-  
-  
-    <hr />
-  
-    <!-- MoreActions -->
-    <div class="btn-group">
-      <button type="button" class="btn btn__primary">Check all</button>
-      <button type="button" class="btn btn__primary">Remove completed</button>
-    </div>
+
+  <hr />
+
+  <!-- MoreActions -->
+  <div class="btn-group">
+    <button type="button" class="btn btn__primary">Check all</button>
+    <button type="button" class="btn btn__primary">Remove completed</button>
   </div>
-  
+</div>
