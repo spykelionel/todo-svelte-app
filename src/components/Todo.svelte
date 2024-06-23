b59da7134a3ca8d26045f0ca4da4c5a1e6dbf313
spykelionel <social.lionel@gmail.com>
2024-06-23 16:54:48 +0100

    create todo component

---
+++ src/components/Todo.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let todo;
+  const dispatch = createEventDispatcher();
+</script>
+
+<div class="stack-small">
+  <div class="c-cb">
+    <input
+      type="checkbox"
+      id="todo-{todo.id}"
+      on:click={() => dispatch("complete", { todo })}
+      checked={todo.completed}
+    />
+    <label for="todo-{todo.id}" class="todo-label">{todo.name}</label>
+  </div>
+  <div class="btn-group">
+    <button type="button" class="btn">
+      Edit <span class="visually-hidden">{todo.name}</span>
+    </button>
+    <button
+      type="button"
+      class="btn btn__danger"
+      on:click={() => dispatch("remove", { todo })}
+    >
+      Delete <span class="visually-hidden">{todo.name}</span>
+    </button>
+  </div>
+</div>
